cfea8e02790949b120e46fa063b65eeba55855df
some egg that does stuff <220542262+theeggswork@users.noreply.github.com>
2026-06-11 20:52:46 -0400

    mmm i did something prob

---
+++ Scripts/Checkpoint.gd
@@ -0,0 +1,16 @@
+extends Area2D
+@onready var player = get_tree().current_scene.get_node("Player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player.checkpointpos = Vector2(position.x,position.y)
