d50582bff683bb5e3031005ee96a599974a26ef0
Egg. <220542262+theeggswork@users.noreply.github.com>
2026-07-21 23:28:39 -0400

    something

--- Scripts/Objects/Background3D.gd
+++ Scripts/Objects/Background3D.gd
@@ -1,5 +1,5 @@
 extends Camera3D
-@onready var player = get_node_or_null("Player")
+@onready var player = get_tree().current_scene.get_node_or_null("Player")
 var i = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if player:
+	if player != null:
 		position = Vector3(player.position.x / 1000,player.position.y / -1000, 0)
 	else:
 		i += 0.01
